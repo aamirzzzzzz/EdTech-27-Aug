2d40170643b8ab4be2a92b6af6a53b9fbd62c65d
aamirzzzzzz <aamirchawre@gmail.com>
2025-08-27 04:16:25 +0530

    Delete main directory

--- main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render , redirect
-from django.contrib.auth import logout
-from courses.models import Course 
-
-def home(request):
-    courses = Course.objects.all()[:6] 
-    return render(request,'accounts/home.html', {'courses': courses})
-
